330bbe94a4b8ec47951e7590199227fef6e1ced1
Rachel Jensen <rachelangela@gmail.com>
2021-06-11 10:30:45 -0600

    Add constructor and checkForWin method

---
+++ game.js
@@ -0,0 +1,58 @@
+class Game {
+  constructor() {
+    this.player1 = new Player();
+    this.player2 = new Player();
+    this.gameboard = {
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9
+    }
+    this.is1sTurn: true;
+    this.is2sTurn: false;
+  }
+
+  checkForWin() {
+    var square = this.gameboard;
+    var winner = '';
+
+    if (square.one === square.two && square.one === square.three) {
+      winner = square.one;
+    }
+    if (square.four === square.five && square.four === square.six) {
+      winner = square.four;
+    }
+    if (square.seven === square.eight && square.seven === square.nine) {
+      winner = square.seven;
+    }
+    if (square.one === square.four && square.one === square.seven) {
+      winner = square.one;
+    }
+    if (square.two === square.five && square.two === square.eight) {
+      winner = square.two;
+    }
+    if (square.three === square.six && square.three === square.nine) {
+      winner = square.three;
+    }
+    if (square.three === square.five && square.three === square.seven) {
+      winner = square.three;
+    }
+    if (square.one === square.five && square.one === square.nine) {
+      winner = square.one;
+    }
+    return winner;
+  };
+
+  checkForDraw() {
+    
+  }
+
+  resetBoard() {
+    this.setTimeout()
+  }
+}
