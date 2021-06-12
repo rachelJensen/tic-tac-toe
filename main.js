68377cac2a3ac9d16c0e7150466d23d7b1818181
Rachel Jensen <rachelangela@gmail.com>
2021-06-12 15:10:15 -0600

    Add token display to game play

--- main.js
+++ main.js
@@ -14,27 +14,23 @@ board.addEventListener('click', function(event) {
 //functions
 
 function play(event) {
-  var chosen = event.target.id;
   setWhosTurn();
-  selectSquare(chosen);
+  selectSquare(event.target.id);
+  displayToken(event.target.id);
 
-  var winner = currentGame.checkForWin();
-  if (winner) {
-    currentGame[winner].wins++;
-    //should activate the saveWinsToStorage method
-    // update the display of the winnings using retrieveWinsFromStorage method
-    //display the winner on the DOM
+  if (currentGame.checkForWin()) {
+    //save win (to storage)
+    //display winnings (from storage)
     window.setTimeout(startNewGame, 3000);
-
   }
 
   var draw = currentGame.checkForDraw();
   if (draw) {
+    //display draw message
     console.log(draw);
-    //rather than returning draw, this should invoke a function that will display the draw message to the DOM
-    //activate the reset timer scope.setTimeout(startNewGame[, 3])
-      // --> does scope refer to the DOM or the data model??
+    window.setTimeout(startNewGame, 3000);
   }
+
 }
 
 function setWhosTurn() {
@@ -45,50 +41,28 @@ function setWhosTurn() {
   }
 }
 
-function selectSquare(square) {
-  // setWhosTurn();
+function selectSquare(squareId) {
   var mark = currentGame.whosTurn;
-  if (typeof currentGame.gameboard[square] === 'number') {
-    currentGame.gameboard[square] = mark;
+  if (typeof currentGame.gameboard[squareId] === 'number') {
+    currentGame.gameboard[squareId] = mark;
     currentGame.playCount++;
   }
 };
 
-function startNewGame() {
-  currentGame.resetBoard(p1,p2);
+function displayToken(squareId) {
+  var squareToMark = document.getElementById(squareId);
+  if (currentGame.gameboard[squareId] === 'player1') {
+    squareToMark.innerHTML = '<img class="icon" src="./assets/1566741.svg" alt="dragon">';
+  } else {
+    squareToMark.innerHTML = '<img class="player2 icon" src="./assets/2023216.svg" alt="unicorn">';
+  }
 }
 
 function displayWinner() {
 
 }
 
-function displayToken() {
-
+function startNewGame() {
+  currentGame.resetBoard(p1,p2);
+  //reset display
 }
-
-
-/* When the page loads, a new game should be ready to be played.
-
-Each click will...
-  - check who's turn it is (game.is1sTurn)
-  - mark the gameboard square(game.gameboard.sqr#) that was clicked with the player who's turn it is
-  - add one to the game.playCount
-  - check for a win
-    - if there's a win, activate the winning function
-  - check for a draw
-    - if there's a draw, activate draw function
-  - switch who's turn
-
-When a win is found
-  - the winner message is displayed
-  - storage is updated with the win
-  - the winning side's points get updated from storage
-  - the display of wins is updated
-  - the gameboard is reassigned to a new instance of game
-  - the game display is reset
-
-When a draw is found
-  - draw message is displayed
-  - gameboard is reassigned to a new instance of gameboard
-  - the game display is reset
-*/
